b07c0532473b569cb6929a7f23ddea8a9c1f691d
ZeynepNurDirik <161508922+ZeynepNurDirik@users.noreply.github.com>
2024-06-03 13:31:17 +0300

    Add files via upload

--- Doktor.py
+++ Doktor.py
@@ -8,13 +8,13 @@ class Doktor(Personel):
         self.__hastane = hastane
 
     def get_uzmanlik(self):
-        return.__uzmanlik
+        return self.__uzmanlik
 
     def get_deneyim_yili(self):
-        return.__deneyim_yili
+        return self.__deneyim_yili
 
     def get_hastane(self):
-        return.__hastane
+        return self.__hastane
 
     def set_deneyim_yili(self):
         self.__deneyim_yili = deneyim_yili
@@ -24,6 +24,16 @@ class Doktor(Personel):
 
     def set_maas_artirma(self,oran):
         self.set_maas(self.get_maas()*(1 + oran))
+
+    def __str__(self):
+        return "{}, Uzmanlık:{}, Deneyim Yılı:{}, Hastane: {}".format(
+            super().__str__(), self.__uzmanlik , self.__deneyim_yili , self.__hastane
+        )
+
+#Doktor classımda personel classından kalıtım aldım.
+#Ek olarak uzmanlık deneyim yılı ve hastane bilgilerini get metoduyla bir sınıfın özel bilgilerine dışarıdan güvenli erişim sağladım.
+#set metodları sınıfın içindeki özelliklerin dışarıdan güvenli bir şekilde değiştirmek için kulanılır.
+#def __str__ metodu print ifadesi ile içindeki bilgileri ekrana yazdırır.
 
 
 
