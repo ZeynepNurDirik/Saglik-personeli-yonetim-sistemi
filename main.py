77d3b0795b03b1ef009068262bf66a5968096bee
Zeynep DİRİK <161508922+ZeynepNurDirik@users.noreply.github.com>
2024-06-05 07:45:48 +0300

    Add files via upload
    main fonksiyonu ana hatlarıyla tamamlandı

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from Hasta import Hasta
 def main():
     try:
 
-        personel1 = Personel(01,"Gizem","Aydın","Resepsiyon",25000)
-        personel2 = Personel(02,"Kerem","Yılmaz","Temizlik",20000)
-        personel3 = Personel(03,"Melisa","Yıldız","Yönetim",50000)
+        personel1 = Personel(1,"Gizem","Aydın","Resepsiyon",25000)
+        personel2 = Personel(2,"Kerem","Yılmaz","Temizlik",20000)
+        personel3 = Personel(3,"Melisa","Yıldız","Yönetim",50000)
 
         print(personel1)
         print(personel2)
@@ -31,7 +31,7 @@ def main():
         hemsire1 = Hemsire(200,"Aslı","Kaya","Acil",35000,
                            40,"Sertifika A","Acıbadem")
 
-        hemsire2 = Hemsire(201,"Raziye","Aydın","Acil",38000,
+        hemsire2 = Hemsire(201,"Raziye","Aydın","Acil",43000,
                            36,"Sertifika C","Medical Point")
 
         hemsire3 = Hemsire(202,"Emine","Tekin","Acil",40000,
@@ -149,6 +149,84 @@ def main():
         }
 
         df = pd.DataFrame(data)
+
+        df.fillna(0,inplace = True) #eksik veriler sıfır ile dolduruldu.
+        return df
+
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
+        return None
+
+def analyze_data(df,secim):
+    try:
+        if secim == "1":
+            doktor_uzmanlik_gruplari = df[df["Uzmanlık"] != 0].groupby("Uzmanlık").size()
+            print(f"Uzmanlık Alanlarına Göre Doktor Sayıları:{doktor_uzmanlik_gruplari}")
+
+        elif secim == "2":
+            deneyimli_doktorlar = df[df["Deneyim Yılı:"] != 0] & (df["Deneyim Yılı:"] > 5)
+            print("\n5 Yıldan Fazla Deneyime Sahip Olan Olan Doktor Sayısı:")
+            print(len(deneyimli_doktorlar))
+
+        elif secim == "3":
+            df_sorted_by_hast_ad = df.sort_values(by ="Ad")
+            print(f"\nHasta Adına Göre Sıralanmış DataFrame:{df_sorted_by_hast_ad}")
+
+        elif secim == "4":
+            maasi_7000_fazla_pers = df[(df["Maas"] != 0) & (df["Maas"] > 7000)]
+            print(f"Maaşı 7000 TL Üzerinde Olan Personeller:{maasi_7000_fazla_pers}")
+
+        elif secim == "5":
+            df["Doğum Tarihi"] = pd.to_datetime(df["Doğum Tarihi"], errors = "coerce")
+            dt_1990_sonrasi_hastalar = df[(df["Doğum Tarihi"] != pd.NaT) & (df["Doğum Tarihi"] >= "1990-01-01")]
+            print(f"Doğum Tarihi 1990 Sonrası Olan Hastalar:{dt_1990_sonrasi_hastalar}")
+
+        elif secim == "6":
+            yeni_data_frame = df[("Ad", "Soyad","Departman","Maas","Uzmanlık","Deneyim Yılı","Hastalık","Tedavi")]
+            print(f"\nYeni DataFrame:{yeni_data_frame}")
+
+        else:
+            print("Geçersiz seçim,lütfen (0-6) arası bir sayı giriniz...")
+
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
+
+def main():
+    while True:
+        print("\n Lütfen yapmak istediğini işlemi seçiniz(Çıkış için 0):")
+        print("1. Uzmanlık Alanlarına Göre Doktor Sayıları")
+        print("2. 5 Yıldan Fazla Deneyime Sahip Olan Doktor Sayısı")
+        print("3. Hasta Adına Göre Sıralanmış DataFrame")
+        print("4. Maaşı 7000 TL Üzrinde Olan Personeller")
+        print("5. Doğum Tarihi 1990 ve Sonrası Olan Hastalar")
+        print("6. Yeni DataFrame Oluştur")
+
+        secim = input("Seçiminiz (0-6):")
+
+        if secim == "6":
+            print("Programndan çıkış yapılıyor.")
+            break
+
+        else:
+            df = main()
+            if df is not None:
+                analyze_data(df , secim)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
